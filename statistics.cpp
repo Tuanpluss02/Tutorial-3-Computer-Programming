38303e5a3e881415ebd05e787d8f694c4f983768
Tuan Plus <Dongoctuan2002kk@gmail.com>
2022-10-05 22:22:32 +0700

    fix variance func

--- statistics.cpp
+++ statistics.cpp
@@ -55,18 +55,18 @@ double standardDeviation::variance(double number)
 
 double standardDeviation::variance(double number1, double number2)
 {
-    double mean = (number1 + number2) / 2;
-    return ((number1 - mean) * (number1 - mean) + (number2 - mean) * (number2 - mean)) / 2;
+    double meanData = mean(number1, number2);
+    return ((number1 - meanData) * (number1 - meanData) + (number2 - meanData) * (number2 - meanData)) / 2;
 }
 
 double standardDeviation::variance(double number1, double number2, double number3)
 {
-    double mean = (number1 + number2 + number3) / 3;
-    return ((number1 - mean) * (number1 - mean) + (number2 - mean) * (number2 - mean) + (number3 - mean) * (number3 - mean)) / 3;
+    double meanData = mean(number1, number2, number3);
+    return ((number1 - meanData) * (number1 - meanData) + (number2 - meanData) * (number2 - meanData) + (number3 - meanData) * (number3 - meanData)) / 3;
 }
 
 double standardDeviation::variance(double number1, double number2, double number3, double number4)
 {
-    double mean = (number1 + number2 + number3 + number4) / 4;
-    return ((number1 - mean) * (number1 - mean) + (number2 - mean) * (number2 - mean) + (number3 - mean) * (number3 - mean) + (number4 - mean) * (number4 - mean)) / 4;
+    double meanData = mean(number1, number2, number3, number4);
+    return ((number1 - meanData) * (number1 - meanData) + (number2 - meanData) * (number2 - meanData) + (number3 - meanData) * (number3 - meanData) + (number4 - meanData) * (number4 - meanData)) / 4;
 }
